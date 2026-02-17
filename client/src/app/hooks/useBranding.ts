33a14669704ee9a16ecada3bfc2f0bcf728da6a3
Bryan Ramos <bramos@redhat.com>
2026-02-17 15:37:01 -0500

    Allow toggling branding when switching modes
    Signed-off-by: Bryan Ramos <bramos@redhat.com>

--- client/src/app/hooks/useBranding.ts
+++ client/src/app/hooks/useBranding.ts
@@ -1,12 +1,40 @@
+import { useMemo } from "react";
 import { type BrandingStrings, brandingStrings } from "@pulp-ui/common";
-
+import { useIsDarkMode } from "./useDarkMode";
 /**
  * Wrap the branding strings in a hook so components access it in a standard
  * React way instead of a direct import.  This allows the branding implementation
  * to change in future with a minimal amount of refactoring in existing components.
  */
 export const useBranding = (): BrandingStrings => {
-  return brandingStrings;
+  const isDark = useIsDarkMode();
+  return useMemo(() => {
+    if (!isDark) return brandingStrings;
+
+    return {
+      ...brandingStrings,
+      masthead: {
+        ...brandingStrings.masthead,
+        leftBrand: brandingStrings.masthead.leftBrand
+          ? {
+            ...brandingStrings.masthead.leftBrand,
+            src:
+              // Use dark mode branding if they exist,
+              // default to original source branding if not.
+              brandingStrings.masthead.leftBrand.darkModeSrc ||
+              brandingStrings.masthead.leftBrand.src,
+
+          }
+          : undefined
+      },
+      about: {
+        ...brandingStrings.about,
+        imageSrc:
+          brandingStrings.about.darkModeImageSrc ||
+          brandingStrings.about.imageSrc,
+      }
+    }
+  }, [isDark]);
 };
 
 export default useBranding;
